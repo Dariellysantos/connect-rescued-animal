f4baf987dfc0963a74b3a34e8a3a40d9d76878b8
dariellysssantos <darielly.santos@creditas.com>
2022-03-29 09:37:54 -0300

    feat: starting the project

---
+++ src/test/kotlin/br/com/connectrescuedanimal/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.connectrescuedanimal.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
