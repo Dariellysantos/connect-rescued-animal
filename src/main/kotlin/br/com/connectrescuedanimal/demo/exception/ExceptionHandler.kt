120acb38e08136217a52e2195a987d0826edc61c
dariellysssantos <darielly.santos@creditas.com>
2022-04-08 11:49:44 -0300

    feat:created exceptions handler not found

---
+++ src/main/kotlin/br/com/connectrescuedanimal/demo/exception/ExceptionHandler.kt
@@ -0,0 +1,27 @@
+package br.com.connectrescuedanimal.demo.exception
+
+import br.com.connectrescuedanimal.demo.dto.ErrorView
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletRequest
+
+@RestControllerAdvice
+class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFound(
+        exception: NotFoundException,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.NOT_FOUND.value(),
+            error = HttpStatus.NOT_FOUND.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+
+}
