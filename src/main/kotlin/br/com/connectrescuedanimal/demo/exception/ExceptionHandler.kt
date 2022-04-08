96e1cfd5f570efc57f0a4f66f385b40d7bc51d50
dariellysssantos <darielly.santos@creditas.com>
2022-04-08 12:37:22 -0300

    feat:created exceptions handler

--- src/main/kotlin/br/com/connectrescuedanimal/demo/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/connectrescuedanimal/demo/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.connectrescuedanimal.demo.exception
 
 import br.com.connectrescuedanimal.demo.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import javax.servlet.http.HttpServletRequest
@@ -23,5 +24,36 @@ class ExceptionHandler {
         )
     }
 
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach { e ->
+            errorMessage[e.field] = e.defaultMessage
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath
+        )
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleServiceError(
+        exception: Exception,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            error = HttpStatus.INTERNAL_SERVER_ERROR.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
 
 }
